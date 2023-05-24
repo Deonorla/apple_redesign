2c46b2064d1a494d34530a0581de30654e418825
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-05-24 09:42:25 +0100

    [Added Product schema in sanity]

---
+++ .history/apple-back/schemas/product_20230524093219.ts
@@ -0,0 +1,94 @@
+import {RiMacbookLine} from 'react-icons/ri'
+import {Rule, SchemaTypeDefinition} from 'sanity'
+
+const product: SchemaTypeDefinition = {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  // icon: RiMacbookLine,
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'array',
+      of: [{type: 'image'}],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      title: 'Category',
+      name: 'category',
+      type: 'reference',
+      to: [{type: 'category'}],
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'blockContent',
+    },
+  ],
+}
+
+export default product
+
+// title: 'Product',
+//   name: 'product',
+//   type: 'document',
+//   // icon: RiMacbookLine,
+//   fields: [
+//     {
+//       title: 'Title',
+//       name: 'title',
+//       type: 'string',
+//       validation: (rule: Rule) => rule.required(),
+//     },
+//     {
+//       title: 'Slug',
+//       name: 'slug',
+//       type: 'slug',
+//       options: {
+//         source: 'title',
+//         maxLength: 100,
+//       },
+//       validation: (rule: Rule) => rule.required(),
+//     },
+//     {
+//       title: 'SKU',
+//       name: 'sku',
+//       type: 'string',
+//     },
+//     {
+//       title: 'Image',
+//       name: 'image',
+//       type: 'image',
+//     },
+//     {
+//       title: 'Parent Product',
+//       name: 'parents',
+//       type: 'array',
+//       of: [{type: 'reference', to: [{type: 'product'}]}],
+//     },
+//   ],
+//   preview: {
+//     select: {title: 'title', media: 'image '},
+//   },
---
+++ apple-back/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wcl00h9z',
+    dataset: 'production'
+  }
+})
