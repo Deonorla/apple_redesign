a01a759dab02fa179c6e4c8d5062b6e50a8f1a95
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-05-30 20:43:52 +0100

    [added cart slice]

--- .history/redux/store_20230529193458.ts
+++ .history/redux/store_20230529193458.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import basketReducer from "./slices/basketSlice";
+
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
