739df49ee5871d962114e3be89988afc0ace57b4
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-12-01 13:08:30 +0100

    Update store.ts

--- redux/store.ts
+++ redux/store.ts
@@ -5,6 +5,5 @@ export const store = configureStore({
     basket: basketReducer,
   },
 });
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
